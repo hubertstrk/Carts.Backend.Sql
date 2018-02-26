5021ccdd34f300269ae7266e109048ff6c9b3fdd
Hubert Stärk <hubert.staerk@gmail.com>
2018-02-26 02:34:43 +0100

    Initial commit

---
+++ migration/initial.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Cart (
+    Id int NOT NULL IDENTITY (1,1),
+    Title NVARCHAR(255),
+    Shop NVARCHAR(255),
+    CreationDate DATETIME,
+    IsDeleted BIT,
+    PRIMARY KEY (Id)
+);
+
+CREATE TABLE CartItem (
+    Id int not null IDENTITY (1,1),
+    CartId int,
+    Name NVARCHAR(255),
+    IsActive BIT,
+    IsDeleted BIT,
+    PRIMARY KEY (Id),
+    FOREIGN KEY (CartId) REFERENCES Cart(Id) ON DELETE CASCADE
+);
---
+++ migration/sample-data.sql
@@ -0,0 +1,12 @@
+INSERT INTO Cart (Title, Shop, CreationDate, IsDeleted) VALUES ('Mit doch egal', 'Penny', '2025-12-10 00:00:00.000', 0);
+INSERT INTO CartItem (CartId, Name, IsActive, IsDeleted) VALUES (1, 'Bananen', '1', 0);
+INSERT INTO CartItem (CartId, Name, IsActive, IsDeleted) VALUES (1, 'Äpfel', '1', 0);
+INSERT INTO CartItem (CartId, Name, IsActive, IsDeleted) VALUES (1, 'Salat', '1', 0);
+INSERT INTO CartItem (CartId, Name, IsActive, IsDeleted) VALUES (1, 'Cornflakes', '1', 0);
+INSERT INTO CartItem (CartId, Name, IsActive, IsDeleted) VALUES (1, 'Nutella', '1', 0);
+
+INSERT INTO Cart (Title, Shop, CreationDate, IsDeleted) VALUES ('Biete Sex für Essen', 'Aldi', '2026-12-10 00:00:00.000', 0);
+INSERT INTO CartItem (CartId, Name, IsActive, IsDeleted) VALUES (3, 'Brot', '1', 0);
+INSERT INTO CartItem (CartId, Name, IsActive, IsDeleted) VALUES (3, 'Milch', '1', 0);
+INSERT INTO CartItem (CartId, Name, IsActive, IsDeleted) VALUES (3, 'Honig', '1', 0);
+INSERT INTO CartItem (CartId, Name, IsActive, IsDeleted) VALUES (3, 'Karotten', '1', 0);
